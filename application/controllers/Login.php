0e2d0b147bf1c97f019b82d09665a9fb01e068d0
criptdestroyer <ahmademir1201@gmail.com>
2020-11-08 06:05:32 +0700

    login logout done

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,7 +5,9 @@ class Login extends MY_Controller {
     {
         parent::__construct();
         $this->load->library('session');
-        // $this->load->model('user_m');
+        $this->load->model('user_m');
+        $this->load->model('log_m');
+
         $this->data['username'] = $this->session->userdata('username');
         $this->data['id_role']  = $this->session->userdata('id_role');
         if (isset($this->data['username'], $this->data['id_role']))
@@ -13,7 +15,7 @@ class Login extends MY_Controller {
             switch ($this->data['id_role'])
             {
                 case 101:
-                    redirect('superadmin');
+                    redirect('Superadmin');
                     break;
 
                 case 201:
@@ -27,19 +29,22 @@ class Login extends MY_Controller {
     public function index()
     {
         if($this->POST('username') && $this->POST('password')) {
-            // if($this->user_m->cek_login(array('username' => $this->POST('username'), 'password' => $this->POST('password')))) {
-            //     // $this->log_m->insert(array('ip_address' => $this->get_ip(), 'waktu' => mdate('%Y-%m-%d %H:%i:%s', now('Asia/Jakarta')), 'username'=>$this->POST('username')));
-            //     if($this->session->userdata('id_role')==1){
-            //         redirect('Admin');
-            //         exit;
-            //     }
-            // } else { 
-            //     echo "<script>alert('Username dan Password salah');window.location = ".json_encode(site_url('login')).";</script>";
-            //     exit;
-            // }
+            if($this->user_m->cek_login(array('username' => $this->POST('username'), 'password' => $this->POST('password')))) {
+                $this->log_m->insert(array('ip_address' => $this->get_ip(), 'waktu' => mdate('%Y-%m-%d %H:%i:%s', now('Asia/Jakarta')), 'username'=>$this->POST('username')));
+                if($this->session->userdata('id_role')==101){
+                    redirect('Superadmin');
+                    exit;
+                }else if($this->session->userdata('id_role')==201){
+                    redirect('Admin');
+                    exit;
+                }
+            } else { 
+                echo "<script>alert('Username dan Password salah');window.location = ".json_encode(site_url('login')).";</script>";
+                exit;
+            }
         }
 
-        $this->data['title'] = "Admin | ";
+        $this->data['title'] = "Login | ";
         $this->data['content'] = "login";
         $this->load->view('login', $this->data);
     }
--- application/models/User_m.php
+++ application/models/User_m.php
@@ -10,6 +10,19 @@ class User_m extends MY_Model {
         $this->data['table_name'] = 'user';
         $this->data['primary_key'] = 'username';
     }
+
+    public function cek_login($user)
+    {
+        $num = $this->get_row(array('username' => $user['username'], 'password' => md5($user['password'])));
+        if(isset($num)) {
+            $array = array(
+                'username' => $num->username,
+                'id_role' => $num->role
+            );
+            $this->session->set_userdata($array);
+            return true;
+        } return false;
+    }
     
 
 }
