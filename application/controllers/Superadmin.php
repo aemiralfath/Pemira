7b57c2d172b30dc9689ad793c6cc838a1c4b32a6
Ryan Duta Pamungkas <38066791+rduta99@users.noreply.github.com>
2020-11-08 01:57:17 +0700

    Change default controller, add login front end & logic

---
+++ application/controllers/Superadmin.php
@@ -0,0 +1,34 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Superadmin extends MY_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+        // $this->load->model('user_m');
+        $this->data['username'] = $this->session->userdata('username');
+        $this->data['id_role']  = $this->session->userdata('id_role');
+        if (isset($this->data['username'], $this->data['id_role']))
+        {
+            if($this->data['id_role'] != 101) {
+                $this->session->sess_destroy();
+                redirect('login');
+                exit;
+            }
+        } else {
+            $this->session->sess_destroy();
+            redirect('login');
+            exit;
+        }
+    }
+
+    public function index()
+    {
+        
+    }
+
+}
+
+/* End of file Superadmin.php */
