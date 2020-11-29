c3f740acccc2a876038282e9ab3e175b87f95ec1
kmhalpin <kmhapinn2@gmail.com>
2020-11-29 13:16:46 +0700

    fix ip track

--- application/controllers/Superadmin.php
+++ application/controllers/Superadmin.php
@@ -299,20 +299,20 @@ class Superadmin extends MY_Controller
     public function get_ip()
     {
         $ipaddress = '';
-        // if (isset($_SERVER['HTTP_CLIENT_IP']))
-        //     $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        // else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        //     $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        // else if (isset($_SERVER['HTTP_X_FORWARDED']))
-        //     $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        // else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
-        //     $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        // else if (isset($_SERVER['HTTP_FORWARDED']))
-        //     $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        // else if (isset($_SERVER['REMOTE_ADDR']))
-        //     $ipaddress = $_SERVER['REMOTE_ADDR'];
-        // else
-        //     $ipaddress = 'UNKNOWN';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if (isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if (isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if (isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
 
         return $ipaddress;
     }
